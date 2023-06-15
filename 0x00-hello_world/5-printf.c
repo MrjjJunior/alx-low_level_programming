954489ebb486437ebc7e16d0ae57b334dc954c11
Tshepiso Tlhong <tlhongtshepiso2@gmail.com>
2023-06-15 06:39:36 -0700

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,13 @@
 #!/bin/bash
+#!/bin/bash
+#include<stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        puts("with proper grammar, but the outcome is a piece of art,");
+        return(0);
+}

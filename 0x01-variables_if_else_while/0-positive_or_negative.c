0e1cf801651e758c8e19dc3fe3f56bf1953d7c69
Tshepiso Tlhong <tlhongtshepiso2@gmail.com>
2023-06-18 12:02:04 -0700

    d

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*deteminees if number is positve or negative*/
+/*main - deteminees if number is positve or negative*/
 /* betty style doc for function main goes there */
 int main(void)
 {

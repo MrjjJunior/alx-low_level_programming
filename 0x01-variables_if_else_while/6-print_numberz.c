1f35d2bc5627cc3c54fb926bae9d3f8818b6e088
Tshepiso Tlhong <tlhongtshepiso2@gmail.com>
2023-06-19 05:09:54 -0700

    d

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -  program that prints single digit numbers of base ten starting from zero
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	putchar('0' + 0);
+	putchar('0' + 1);
+	putchar('0' + 2);
+	putchar('0' + 3);
+	putchar('0' + 4);
+	putchar('0' + 5);
+	putchar('0' + 6);
+	putchar('0' + 7);
+	putchar('0' + 8);
+	putchar('0' + 9);
+	putchar('\n');
+
+	return (0);
+}

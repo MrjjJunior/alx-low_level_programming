648addb86f57ed6a9544ec272fa775689d9510e9
Tshepiso Tlhong <tlhongtshepiso2@gmail.com>
2023-06-19 06:19:12 -0700

    d

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for (num2 = 0; num2 < 10; num2++)
+		{
+			putchar(num1 + '0');
+			putchar(num2 + '0');
+
+			if (num1 != 9 || num2 != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n);
+
+	return 0;
+}

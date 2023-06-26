6e3d151f4d8cc93e318c3dc178ce45159169f8a0
Tshepiso Tlhong <tlhongtshepiso2@gmail.com>
2023-06-26 06:57:49 -0700

    d

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - t prints a string, in reverse
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+	int w;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	s--;
+	for (w = len; w > 0; w--)
+	{
+		putchar(*s);
+		s--;
+	}
+
+	putchar('\n')
+}

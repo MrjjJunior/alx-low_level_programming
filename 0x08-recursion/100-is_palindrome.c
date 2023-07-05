b17818c93ece31418cbb54e04cdee0a0a3d70494
Tshepiso Tlhong <tlhongtshepiso2@gmail.com>
2023-07-05 06:51:23 -0700

    d

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -13,7 +13,7 @@ int is_palindrome_helper(char *s, int start, int end)
 	if (s[start] != s[end])
 		return (0);
 
-	return is_palindrome_helper(s, start + 1, end - 1);
+	return (is_palindrome_helper(s, start + 1, end - 1));
 }
 
 /**
@@ -28,5 +28,5 @@ int is_palindrome(char *s)
 	if (len <= 1)
 		return (1);
 
-	return is_palindrome_helper(s, 0, len - 1);
+	return (is_palindrome_helper(s, 0, len - 1));
 }

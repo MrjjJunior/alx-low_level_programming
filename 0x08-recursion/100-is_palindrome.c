3df20c0b38625ee7c7f05487a89ef03eee678bd9
Tshepiso Tlhong <tlhongtshepiso2@gmail.com>
2023-07-05 06:49:15 -0700

    done

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * is_palindrome_helper - check if a string is a palindrome.
+ *
+ * Return: 1 if the string is a palindrome, otherwise 0
+ */
+int is_palindrome_helper(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+	if (s[start] != s[end])
+		return (0);
+
+	return is_palindrome_helper(s, start + 1, end - 1);
+}
+
+/**
+ * is_palindrome - checks if a string is a palindrome.
+ *
+ * Return: 1 if the string is a palindrome, otherwise 0.
+ */
+int is_palindrome(char *s)
+{
+	int len = strlen(s);
+
+	if (len <= 1)
+		return (1);
+
+	return is_palindrome_helper(s, 0, len - 1);
+}

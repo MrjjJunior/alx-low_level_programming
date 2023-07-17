3f26fca353f6a22b3072fd707c7f711da17bcc87
Tshepiso Tlhong <tlhongtshepiso2@gmail.com>
2023-07-17 10:19:28 -0700

    d

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,13 +15,13 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+};
 /**
  * dog_t - typedef for struct dog
  */
 typedef struct dog dog_t;
 
-void int_dog(struct *d, char *name, float age,char *owner);
+void int_dog(struct dog *d, char *name, float age,char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);

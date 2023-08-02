6c6a63a01d728fa237122bd06fda8a54906209e9
Tshepiso Tlhong <tlhongtshepiso2@gmail.com>
2023-08-02 00:47:57 -0700

    done

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,24 @@
+#include <stddef.h>
+
+/**
+ * listint_len - returns the number of elements in a linked
+ * @h: linked list
+ * @next: points to the next node
+ *
+ * Return: number of nodes
+ *
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count++;
+
+		h = h->next;
+	}
+
+	return (count);
+}

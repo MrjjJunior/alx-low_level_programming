74bde8ae3d1d05d4b13e3a922c3df397f3f2ee28
Tshepiso Tlhong <tlhongtshepiso2@gmail.com>
2023-08-22 14:43:56 -0700

    file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,12 +6,12 @@ char *make_buffer(void);
 void close_file(int fd);
 
 /**
- * make_buffer - returns a buffer of size 1024 bytes.
+ * create_buffer - returns a buffer of size 1024 bytes.
  *
  * Return: A pointer to the new allocated buffer.
  */
 
-char *make_buffer(void)
+char *create_buffer(void)
 {
 	char *buffer = malloc(1024);
 
@@ -21,63 +21,45 @@ char *make_buffer(void)
 		exit(EXIT_FAILURE);
 	}
 
-	return (buffer);
+	return buffer;
 }
 /**
  * close_file - closes selected file descriptor
  *
  * @fd: the file descriptor to be closed.
  */
-
-void close_file(int fd)
+lose_file(int fd)
 {
 	if (close(fd) == -1)
 	{
-		perror("Error: Unable to close file descriptor");
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(EXIT_FAILURE);
 	}
 }
-
 int main(int argc, char *argv[])
 {
-	int from_fd, to_fd, bytes_read, bytes_written;
+	int from_fd, to_fd, r, w;
 	char *buffer;
 
 	if (argc != 3)
 	{
-		fprintf(stderr, "Usage: %s file_from file_to\n", argv[0]);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(EXIT_FAILURE);
 	}
 
-	to_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (to_fd == -1)
+	buffer = create_buffer(argv[2]);
+	from_fd = open(argv[1], O_RDONLY);
+	to_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, FILE_PERMS);
+	while ((r = read(from_fd, buffer, BUFFER_size)) > 0)
 	{
-		perror("Error: Unable to creat or open destination file");
-		free(buffer);
-		close_file(from_fd);
-		exit(EXIT_FAILURE);
-	}
-
-	while ((bytes_read = read(from_fd, buffer, 1024)) > 0)
-	{
-		bytes_written = write(to_fd, buffer, bytes_read);
-		if (bytes_written == -1)
+		if (from_fd == -1 || r == -1)
 		{
-			perror("Error: Unable to write to destinatiom file");
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
-			close_file(from_fd);
-			close_file(to_fd);
 			exit(EXIT_FAILURE);
 		}
 	}
-	if (bytes_read == -1)
-	{
-		perror("Error: Unable to read from source file");
-		free(buffer);
-		close_file(from_fd);
-		close_file(to_fd);
-		exit(EXIT_FAILURE);
-	}
+
 	free(buffer);
 	close_file(from_fd);
 	close_file(to_fd);

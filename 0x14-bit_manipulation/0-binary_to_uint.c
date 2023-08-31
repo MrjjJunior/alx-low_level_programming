6394a3552f09a5890a79beb868a3e51154f50a32
Tshepiso Tlhong <tlhongtshepiso2@gmail.com>
2023-08-31 07:01:48 -0700

    binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  *
@@ -12,7 +13,7 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
 
-	if (b == 0)
+	if (b == NULL)
 		return (0);
 
 	for (; *b; b++)
